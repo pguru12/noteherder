794b44deca14cb9b50af9be477c4ad64cce30c87
Pranav <pguru12@gmail.com>
2017-06-15 11:43:17 -0400

    better

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 import './NoteForm.css'
 
-class NoteForm extends React.Component {
-    render(){
-        return <p>NOTE FORM!</p>
-    }
+const NoteForm = () => {
+    return (
+    <div class="NoteForm">
+        <form>
+          <p>
+            <input type="text" name="title" placeholder="Title your note" value=""/>
+          </p>
+          <p>
+            <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+          </p>
+        </form>
+      </div>
+    )
 }
 export default NoteForm
